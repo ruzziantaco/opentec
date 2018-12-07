029c642dfb487890cfcdbd7c7c3b81e6f97c2c0a
russo <ruzziantaco@gmail.com>
2018-12-06 23:57:27 -0800

    I THINK I GOT IT BITCH

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -135,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATICFILES_DIRS = (
-#   os.path.join(PROJECT_ROOT, 'static'),
-#)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (
+   os.path.join('/home/russo/proyectfinal-opentec/core','static'),
+)
 
 
 # Defined media files
